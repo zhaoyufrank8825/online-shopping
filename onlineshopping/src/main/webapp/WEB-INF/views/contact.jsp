4925b937193d4d4d70447ce523a434f551739df3
zhao yu <zhaoyufrank8825@gmail.com>
2018-04-20 13:42:17 -0500

    Completed the interface theme and other issues!

---
+++ onlineshopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,9 @@
+<div class="container">
+	<div class="row">
+		<h1>Contact Us</h1>
+		<hr />
+		<p>This is a contact us page</p>
+
+	</div>
+
+</div>
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,16 @@
+$(function(){
+	switch(menu){
+	
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	default:
+		$('#home').addClass('active');
+		break;
+		
+		
+	}
+});
